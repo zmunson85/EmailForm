0901321a42cfde8a5abef5077db89a29b76bda0a
Zach Munson <zmunson85@gmail.com>
2021-08-20 01:23:11 -0600

    index.js, HeaderComponent, ContactForm

---
+++ client/src/components/ContactForm.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ContactForm = () => {
+    return (
+        <div>
+            <h2>Contact Form</h2>
+        </div>
+    );
+};
+
+export default ContactForm;
--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Header from './components/Header';
+import ContactForm from './components/ContactForm';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './style.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class App extends React.Component {
+    render() {
+        return (
+            <div className="container">
+                <div className="col-md-6 offset-md-3">
+                    <Header />
+                    <ContactForm />
+                </div>
+            </div>
+        );
+    }
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<App />, document.getElementById('root'));
